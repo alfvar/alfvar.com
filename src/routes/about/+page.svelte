f53258599c111d38634bb4f010f0bca45d361c12
alfvar <alfvar.arvidsson@gmail.com>
2022-11-01 19:37:59 +0100

    Small fixes on About

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,9 @@
 <div class="wrapper">
 	<h2 class="header">Nice to meet you!</h2>
 
-	<div style="display:flex; ">
+	<div>
+		<img src={selfie} alt="Alfvar" class="drawing" />
+
 		<p>
 			My name is Alfvar Arvidsson. I'm a budding <b>UX designer</b> currently doing my bachelor's
 			degree at Halmstad University. I'm interested in
@@ -15,26 +17,31 @@
 			if you want to get in touch.
 			<br />
 			<br />
-			I love working in teams. I was the guy who went to school on the days when I was finished with my assignment,
-			just to hang with my classmates. I tend to get visions of ideas quickly but I always make sure the
-			team is on board and understands what I mean before proceeding with anything.
+			I love working in teams. I was the guy who went to school on the days when I was finished with
+			my assignment, just to hang with my classmates. I tend to get visions of ideas quickly but I always
+			make sure the team is on board and understands what I mean before proceeding with anything.
 			<br />
 			<br />
-			I've barely ever found anything that doesn't capture my interest (except sports!). I grew up playing with a digital
-			camcorder and Windows Movie Maker, and I focused on different forms of media production all through
-			high school.
+			I've barely ever found anything that doesn't capture my interest (except sports!). I grew up playing
+			with a digital camcorder and Windows Movie Maker, and I focused on different forms of media production
+			all through high school. I used to produce music under the alias
+			<a
+				href="https://open.spotify.com/artist/36vb4wCIqT2TjjXLEBmn0m?si=qttRnaeqR1umoEmiHYr11g"
+				class="p">alfie.</a
+			>, and I've dedicated a fair share of my spare time to 3D modelling.
 		</p>
-
-		<img src={selfie} alt="Alfvar" class="drawing" />
 	</div>
 </div>
 
 <style>
 	.drawing {
-		margin-top:-0.5rem;
+		float: right;
+		margin-top: -2rem;
 		width: 12rem;
 		height: 20rem;
 		border-radius: 50%;
 		margin-right: -1rem;
+		margin-bottom: 1rem;
 	}
+
 </style>
